cec8a82f1d54d3d1168de886e16b4099a8ded32b
0xngmi <0xngmi@protonmail.com>
2022-08-22 16:08:11 +0100

    change contract for tubby cats mainnet

--- contracts/LendingPool.sol
+++ contracts/LendingPool.sol
@@ -18,7 +18,7 @@ contract LendingPool is Ownable, ERC721 {
     }
 
     IERC721 public constant nftContract =
-        IERC721(0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b);
+        IERC721(0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD);
     uint256 public constant maxLoanLength = 2 weeks;
     uint256 public constant maxInterestPerEthPerSecond = 25367833587; // ~ 0.8 ether / 1 years;
     uint256 public maxPrice;
