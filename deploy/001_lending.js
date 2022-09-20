97ae0100ea229f52356755bdd8a30de391e7e23e
0xngmi <0xngmi@protonmail.com>
2022-09-20 01:36:22 +0100

    update deployment params

--- deploy/001_lending.js
+++ deploy/001_lending.js
@@ -10,7 +10,12 @@ const func = async function (hre) {
 
   await deploy('LendingPool', {
     from: deployer,
-    args: ["0xab2F947D22ab9CCFC34c9d257fce971c05042b59", "60000000000000000", "0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD"], // 0.06 eth
+    args: [
+      "0x4096b3f0e89c06e98d1095da7aefdd4b38eeb1e0",
+      "60000000000000000", // 0.06 eth
+      "0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD", // tubby cats
+      "1000000000000000000", // 1 eth
+    ],
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     //deterministicDeployment: true,
