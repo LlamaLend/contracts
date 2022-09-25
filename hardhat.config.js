b89843629f788d5f8b335a0ac302225532df27db
0xngmi <0xngmi@protonmail.com>
2022-09-25 03:05:28 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,49 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require('dotenv').config();
+require('hardhat-deploy');
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.7",
+    ...(process.env.DEPLOY === "true" &&
+    {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 999999,
+        },
+      },
+    }
+    )
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.GOERLI_RPC
+      }
+    },
+    mainnet: {
+      url: process.env.ETH_RPC,
+      accounts: [process.env.PRIVATEKEY],
+      gasMultiplier: 1.5,
+    },
+    kovan: {
+      url: process.env.KOVAN_RPC,
+      accounts: [process.env.PRIVATEKEY],
+      gasMultiplier: 1.5,
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC,
+      accounts: [process.env.PRIVATEKEY],
+      gasMultiplier: 1.5,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN
+  },
 };
