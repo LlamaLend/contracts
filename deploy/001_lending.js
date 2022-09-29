66119f81320d26d40015318380ee69eca3ac035f
0xngmi <0xngmi@protonmail.com>
2022-09-29 16:32:26 +0100

    deploy sample pool

--- deploy/001_lending.js
+++ deploy/001_lending.js
@@ -1,7 +1,6 @@
-/*
 const func = async function (hre) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
+  const {deployments, getNamedAccounts, ethers} = hre;
+  const {deployIfDifferent, log} = deployments;
   
   if(process.env.DEPLOY !== "true"){
     throw new Error("DEPLOY env var must be true")
@@ -9,24 +8,26 @@ const func = async function (hre) {
 
   const {deployer} = await getNamedAccounts();
 
-  await deploy('LendingPool', {
-    from: deployer,
-    args: [
-      "0x4096b3f0e89c06e98d1095da7aefdd4b38eeb1e0",
-      "60000000000000000", // 0.06 eth
-      "0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD", // tubby cats
-      "1000000000000000000", // 1 eth
-      "TubbyLoan",
-      "TL",
-      "1209600", // 2 weeks
-      "25367833587", // 80% p.a.
-    ],
-    log: true,
-    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
-    //deterministicDeployment: true,
-  });
+  const LlamaLendFactoryDeployment = await deployments.get('LlamaLendFactory');
+  const LlamaLendFactory = await ethers.getContractFactory("LlamaLendFactory");
+  const factory = await LlamaLendFactory.attach(LlamaLendFactoryDeployment.address)
+
+  const deployResult = await (await factory.createPool(
+    "0x4096b3f0e89c06e98d1095da7aefdd4b38eeb1e0",
+    "60000000000000000", // 0.06 eth
+    "0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD", // tubby cats
+    "1000000000000000000", // 1 eth
+    "TubbyLoan",
+    "TL",
+    "1209600", // 2 weeks
+    "25367833587", // 80% p.a.
+    "12683916793", // 40% p.a.
+    {
+      from: deployer
+  })).wait()
+
+  log(`contract LendingPool deployed at ${deployResult.events[2].args.pool}`);
 };
 module.exports = func;
 func.tags = ['LendingPool'];
 func.dependencies = ['LlamaLendFactory'];
-*/
