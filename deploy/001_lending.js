0f1ba09d5112209985ef02a9b85828b35b466d36
0xngmi <0xngmi@protonmail.com>
2022-10-03 19:14:14 +0100

    deploy with different params on testnet

--- deploy/001_lending.js
+++ deploy/001_lending.js
@@ -12,21 +12,34 @@ const func = async function (hre) {
   const LlamaLendFactory = await ethers.getContractFactory("LlamaLendFactory");
   const factory = await LlamaLendFactory.attach(LlamaLendFactoryDeployment.address)
 
-  const deployResult = await (await factory.createPool(
-    "0x4096b3f0e89c06e98d1095da7aefdd4b38eeb1e0",
+  const chainId = await (await ethers.getSigner()).getChainId()
+  const isTestnet = chainId !== 1;
+
+  const params = [
+    isTestnet? "0x70997970C51812dc3A010C7d01b50e0d17dc79C8" // Fake oracle
+    :"0x4096b3f0e89c06e98d1095da7aefdd4b38eeb1e0", // Real oracle
     "60000000000000000", // 0.06 eth
-    "0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD", // tubby cats
+    isTestnet?
+       "0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b"  // MultiFaucet NFT
+      :"0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD", // tubby cats
     "1000000000000000000", // 1 eth
     "TubbyLoan",
     "TL",
     "1209600", // 2 weeks
     "25367833587", // 80% p.a.
     "12683916793", // 40% p.a.
+  ]
+
+  const deployResult = await (await factory.createPool(
+    ...params,
     {
       from: deployer
-  })).wait()
+    }
+  )).wait()
 
-  log(`contract LendingPool deployed at ${deployResult.events[2].args.pool}`);
+  const poolAddress = deployResult.events[2].args.pool
+  log(`contract LendingPool deployed at ${poolAddress}`);
+  log(`You can verify it on etherscan by running "npx hardhat verify --network goerli ${poolAddress} ${params.join(" ")} ${deployer}"`);
 };
 module.exports = func;
 func.tags = ['LendingPool'];
