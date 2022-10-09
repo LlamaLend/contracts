f4fed9db89163939f165de4c470d503f22aa820b
Justin Silver <jsilver@doublesharp.com>
2022-10-09 14:21:02 -0700

    fix token ID bug
    mock was using the iterator as the token ID preventing it from minting more than once.

--- contracts/mocks/MockNFT.sol
+++ contracts/mocks/MockNFT.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockNFT is ERC721 {
-    constructor() ERC721("Mock NFT", "MOCK"){}
+    uint256 private _currentTokenId;
 
-    function mint(uint amount, address to) external {
-        for(uint i = 0; i<amount; i++){
-            _mint(to, i);
+    constructor() ERC721("Mock NFT", "MOCK") {}
+
+    function mint(uint256 amount, address to) external {
+        for (uint256 i = 0; i < amount; i++) {
+            _mint(to, ++_currentTokenId);
         }
     }
 }
