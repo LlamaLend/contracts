2f86e8a1cd87e5a9e5bbbcc766a88956a258b4e0
Justin Silver <jsilver@doublesharp.com>
2022-10-09 14:27:07 -0700

    ListNfts for balances > 100
    Update ListNfts to use the erc721.balanceOf(address) to size the dynamic array, then chop off any empty spots using assembly.
    
    Updated tests/coverage.
    
    Note: this is a breaking change as the length is no longer returned.

--- contracts/ListNfts.sol
+++ contracts/ListNfts.sol
@@ -4,19 +4,36 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract ListNfts {
-    function getOwnedNfts(address owner, IERC721 nftContract, uint start, uint end) external view returns (uint[100] memory nfts, uint length){
-        length = 0;
-        while(start < end){
-            try nftContract.ownerOf(start) returns (address nftOwner) { 
-                if(nftOwner == owner){
+    function getOwnedNfts(
+        address owner,
+        IERC721 nftContract,
+        uint256 start,
+        uint256 end
+    ) external view returns (uint256[] memory nfts) {
+        // make sure the array can hold all the possible tokens
+        uint256 balance = nftContract.balanceOf(owner);
+        // we can't have more than the balance
+        nfts = new uint256[](balance);
+        uint256 length = 0;
+        while (start < end) {
+            try nftContract.ownerOf(start) returns (address nftOwner) {
+                if (nftOwner == owner) {
                     nfts[length] = start;
                     unchecked {
+                        // unchecked: always less than end minus start
                         length++;
                     }
                 }
             } catch {}
             unchecked {
+                // unchecked: start is always less than end
                 start++;
+            }
+        }
+        // resize the array to the found number of token ids
+        if (length != nfts.length) {
+            assembly {
+                mstore(nfts, length)
             }
         }
     }
--- test/listNfts.js
+++ test/listNfts.js
@@ -1,18 +1,51 @@
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("ListNfts", function () {
-  it("oracle", async function () {
-    this.timeout(1000000);
-    const ListNfts = await ethers.getContractFactory("ListNfts");
+describe('ListNfts', function () {
+  let mockNFT, listNfts, owner, other;
+  beforeEach(async function () {
+    [owner, other] = await ethers.getSigners();
 
-    const listNfts = await ListNfts.deploy();
+    const MockNFT = await ethers.getContractFactory('MockNFT');
+    mockNFT = await MockNFT.deploy();
+    await mockNFT.deployed();
+
+    // quantity, recipient
+    await mockNFT.mint(2, owner.address);
+    await mockNFT.mint(3, other.address);
+    await mockNFT.mint(100, owner.address);
+
+    const ListNfts = await ethers.getContractFactory('ListNfts');
+    listNfts = await ListNfts.deploy();
     await listNfts.deployed();
+  });
+
+  it('full range', async function () {
+    this.timeout(1000000);
 
     const owners = await listNfts.getOwnedNfts(
-      "0x50664ede715e131f584d3e7eaabd7818bb20a068", //yfimaxi.eth
-      listNfts.address, 
-      0, 
+      owner.address,
+      mockNFT.address,
+      1,
       5e3
     );
+
+    // all of them should have been returned
+    const balance = await mockNFT.balanceOf(owner.address);
+    expect(owners.length).to.equal(balance);
+  });
+
+  it('partial range', async function () {
+    this.timeout(1000000);
+
+    const owners = await listNfts.getOwnedNfts(
+      owner.address,
+      mockNFT.address,
+      1,
+      101
+    );
+
+    // in the range 1-100 inclusive three were minted to a different user
+    expect(owners.length).to.equal(97);
   });
 });
