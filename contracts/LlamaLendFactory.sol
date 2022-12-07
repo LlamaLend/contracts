b3af0c74c5ae03fb30acb3923ccca8215c999690
0xngmi <0xngmi@protonmail.com>
2022-12-07 12:05:18 +0000

    deposit on pool creation

--- contracts/LlamaLendFactory.sol
+++ contracts/LlamaLendFactory.sol
@@ -21,10 +21,11 @@ contract LlamaLendFactory is Ownable {
         address _oracle, uint _maxPrice, address _nftContract, 
         uint _maxDailyBorrows, string memory _name, string memory _symbol,
         uint96 _maxLoanLength, LendingPool.Interests calldata interests
-    ) external returns (LendingPool pool) {
+    ) external payable returns (LendingPool pool) {
         require(_maxLoanLength < 1e18, "maxLoanLength too big"); // 31bn years, makes sure that reverts cant be forced through this
         pool = LendingPool(address(implementation).clone());
         pool.initialize(_oracle, _maxPrice, _maxDailyBorrows, _name, _symbol, interests, msg.sender, _nftContract, address(this), _maxLoanLength);
+        payable(pool).sendValue(msg.value);
         emit PoolCreated(_nftContract, msg.sender, address(pool));
     }
 
